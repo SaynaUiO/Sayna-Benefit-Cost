daeb53b8818b90bf49be1858e5ecc544b5bac279
saynag <saynag.uio.no>
2025-10-29 17:44:34 +0100

    fix

--- src/resolvers/OnboardingResolver.ts
+++ src/resolvers/OnboardingResolver.ts
@@ -2,12 +2,16 @@ import { storage } from "@forge/api";
 import Resolver from "@forge/resolver";
 import { getIsOnboardingCompleted, setIsOnboardingCompleted } from "../services/OnboardingService";
 
+interface SetOnboardingPayload {
+    onboardingComplete: boolean;
+}
+
 export const onboardingResolver = (resolver: Resolver) => {
     resolver.define('isOnboardingComplete', async ({ context }): Promise<boolean> => {
         return getIsOnboardingCompleted(context.accountId, context.extension.project.id)
     })
 
-    resolver.define('setOnboardingComplete', async ({ context, payload }) => {
+    resolver.define('setOnboardingComplete', async ({ context, payload }: { context: any, payload: SetOnboardingPayload }) => {
         setIsOnboardingCompleted(context.accountId, payload.onboardingComplete, context.extension.project.id)
     })
 }
